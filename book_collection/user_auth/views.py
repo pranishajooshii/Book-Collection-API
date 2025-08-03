2210404f1ebdd3ef4cdaec2f1c205183cfb9f275
Pranisha Joshi <pranisha.joshi11@gmail.com>
2025-08-03 20:57:26 +0545

    feat(user_auth):created views

--- book_collection/user_auth/views.py
+++ book_collection/user_auth/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
+from .serializers import RegisterSerializer, LoginSerializer
+from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from django.contrib.auth import get_user_model
+from rest_framework.decorators import api_view, permission_classes
 
-# Create your views here.
+User = get_user_model()
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def register(request):
+   
+        serializer=RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            user=serializer.save()
+            return Response({"message":"User registered successfully","user":serializer.data}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def login(request):
+      serializer=LoginSerializer(data=request, context={'request': request})
+      if serializer.is_valid():
+            user=serializer.validated_data['user']
+            token, created= Token.objects.get_or_create(user=user)  
+            return Response({
+                "message": "Login successful",
+                "user": {
+                    "email": user.email,
+                    "fullname": user.fullname,
+                },
+                "token": token.key
+            }, status=status.HTTP_200_OK)
+      return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
